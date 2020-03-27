274daafa250b5cabdbe3c2fb26afa6fa56248c16
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-27 22:42:48 +0530

    Constructor

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,15 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+import problem1.node.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    static Queue<TreeNode> q;
+    //constructor
+
 }
