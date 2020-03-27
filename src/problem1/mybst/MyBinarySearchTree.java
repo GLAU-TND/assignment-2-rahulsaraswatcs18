65798086dbfa16d9d9791f8926bc9d2c912506c3
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-27 22:46:27 +0530

    Defining method for count node without left node

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -35,5 +35,35 @@ public class MyBinarySearchTree {
         }
     }
 
+    public void countNodeWithOutLeftChild(TreeNode root) {
+        // If tree is empty
+        if (root == null)
+            return;
+
+        // Do level order traversal starting from root
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+
+        int count = 0; // Initialize count of half nodes
+        while (queue != null) {
+            TreeNode temp = queue.poll();
+            if (temp == null) {
+                break;
+            }
+
+            if (temp.getLeft() == null)
+                count++;
+
+            // Enqueue left child
+            if (temp.getLeft() != null)
+                queue.add(temp.getLeft());
+
+            // Enqueue right child
+            if (temp.getRight() != null)
+                queue.add(temp.getRight());
+        }
+        System.out.println("count of Nodes without left child: " + count);
+    }
+
 
 }
