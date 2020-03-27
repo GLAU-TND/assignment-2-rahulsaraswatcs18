53d19ccb2a158261e1566e6a9834d8c40923d0e2
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-27 22:37:53 +0530

    Initializing variables

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,5 +7,10 @@
 package problem1.node;
 
 public class TreeNode {
+    //variabales to store data and refrences
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
 
 }
