33f19badc2b4707233746c5770fc6f4c951fb217
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-28 08:33:58 +0530

    Defining constructor

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -17,7 +17,19 @@ public class Student {
     private int rollno, backlog, apperingcount;
     private Scanner sc;
 
+    public Student() {
+        sc = new Scanner(System.in);
+        System.out.print("Name :");
+        this.name = sc.next();
+        System.out.print("Roll.no : ");
+        this.rollno = sc.nextInt();
+        System.out.print("Backlog_count : ");
+        this.backlog = sc.nextInt();
+        System.out.print("appering_count");
+        this.apperingcount = sc.nextInt();
 
+
+    }
 
 
 }
