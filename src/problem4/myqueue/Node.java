72b698de9579206d37db1bdf1ca0c39cb921f360
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-28 08:22:08 +0530

    Defining getters and setters

--- src/problem4/myqueue/Node.java
+++ src/problem4/myqueue/Node.java
@@ -12,7 +12,26 @@ public class Node {
         next = null;
     }
 
+    public Node(TreeNode treenode) {
+        node = treenode;
+        next = null;
+    }
 
+    public TreeNode getNode() {
+        return node;
+    }
+
+    public void setNode(TreeNode node) {
+        this.node = node;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 
 
 }
