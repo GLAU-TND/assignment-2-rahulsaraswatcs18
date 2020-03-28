9ba7e257b816ab5417e1b289b7b19fed2b365086
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-28 08:27:43 +0530

    Defining method printSuccessor

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -96,6 +96,19 @@ public class MyQueue {
     }
 
 
+    public void printSuccessor(int data) {
 
+        tmp = front;
+        while (tmp.getNode().getData() != data && tmp != null) {
+            tmp = tmp.getNext();
+        }
+        try {
+            assert tmp != null;
+            System.out.println(tmp.getNext().getNode().getData());
+        } catch (NullPointerException ignore) {
+            System.out.println("No preorder Successor found");
+        }
+
+    }
 
 }
