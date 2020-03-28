5501aa5cd68f44e1861a876bae2255207d2ff7c8
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-28 08:31:23 +0530

    Defining main method

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -21,6 +21,25 @@ public class MyMain {
         Scanner sc = new Scanner(System.in);
         MyBinarySearchTree m = new MyBinarySearchTree();
 
+        MyQueue q = new MyQueue();
+
+        TreeNode root;
+        root = new TreeNode(10);
+        m.insert(root, 5);
+        m.insert(root, 1);
+        m.insert(root, 15);
+        m.insert(root, 9);
+        m.insert(root, 7);
+        m.insert(root, 12);
+        m.insert(root, 30);
+        m.insert(root, 25);
+        m.insert(root, 40);
+        m.insert(root, 45);
+        m.insert(root, 6);
+        q.preOrder(root);
+        q.queuePrint(q);
+        System.out.print("Enter value of which you want to find preorder Successor : ");
+        q.printSuccessor(sc.nextInt());
 
 
 }
