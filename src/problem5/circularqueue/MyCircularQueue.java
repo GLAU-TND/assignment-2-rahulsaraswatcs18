bfa99d29e1f1543de5d0874ad845ef1ce721d103
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-28 08:43:13 +0530

    Defining method for printing queue

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -50,4 +50,17 @@ public class MyCircularQueue {
 
     }
 
+    public void printQueue() {
+        tmp = front;
+        try {
+            do {
+                System.out.println(tmp.getS().toString());
+                tmp = tmp.getNext();
+            }
+            while (tmp != front && tmp != null);
+        } catch (NullPointerException ignored) {
+        }
+    }
+
+
 }
