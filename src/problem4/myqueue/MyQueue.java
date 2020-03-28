0cb34311f4177324923403488e254add98941874
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-28 08:23:20 +0530

    Defining variables

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -5,7 +5,17 @@
  *  Time: 7:11 PM
  */
 package problem4.myqueue;
+
+import problem1.node.TreeNode
+
 // to create queue to store pre - order successor
 
 public class MyQueue {
+
+    private Node front, end, tmp;
+    private int size;
+
+
+
+
 }
