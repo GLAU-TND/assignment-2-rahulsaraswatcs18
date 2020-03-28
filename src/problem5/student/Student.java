e3293a787be89b2356256b73317e49d7ba8104d6
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-28 08:35:00 +0530

    Defining getters and setters

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -30,6 +30,38 @@ public class Student {
 
 
     }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(int backlog) {
+        this.backlog = backlog;
+    }
+
+    public int getApperingcount() {
+        return apperingcount;
+    }
+
+    public void setApperingcount(int apperingcount) {
+        this.apperingcount = apperingcount;
+    }
+
 
 
 }
