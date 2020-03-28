f6f79cf7ccb6f316c6bc577721e3ca65532a047b
RahulSaraswat23 <53719130+rahulsaraswatcs18@users.noreply.github.com>
2020-03-28 08:11:11 +0530

    Defining getters and setters

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -17,4 +17,31 @@ public class Node {
         this.priority = priority;
     }
 
+
+    //getter and setter..........
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
 }
